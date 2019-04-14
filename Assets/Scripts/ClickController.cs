dbc965dec86cbf8d27536651f349155cf11e664d
sanya113 <sanya113@github.com>
2019-04-14 10:40:50 +0300

    first commit

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickController : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnPointerDown(PointerEventData data)
+    {
+        transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+        MoneyControl.AddMoney(1);
+    }
+    public void OnPointerUp(PointerEventData data)
+    {
+        transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+    }
+}
---
+++ Assets/Scripts/MoneyControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyControl : MonoBehaviour // объявляем класс
+{
+    public float Money = 0; // создаём переменную
+    public Text MoneyText;
+    public static MoneyControl MoneyScript;
+    private void Start()
+    {
+        MoneyScript = this;
+    }
+    private void Update()
+    {
+       MoneyText.text = "Money: " + MoneyScript.Money;
+    }
+    public static void AddMoney(float Value) // статическа функция, которая добавляет к числу Money
+    {
+        MoneyScript.Money += Value;
+    }
+    public static void SubMoney (float Value) // статическая Функция, которая вычитает у Money
+    {
+        MoneyScript.Money -= Value;
+    }
+    public static float GetMoney() // Статическая функция, которая возвращает значение Money
+    {
+        return MoneyScript.Money;
+    }
+}
